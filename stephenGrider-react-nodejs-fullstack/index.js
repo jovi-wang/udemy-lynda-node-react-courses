9fb08a7bbb875ce4a5e6e3e7d78908bcfb2e3b48
jovi <jovi_wang@outlook.com>
2019-03-09 19:05:10 +1100

    add comment

--- stephenGrider-react-nodejs-fullstack/index.js
+++ stephenGrider-react-nodejs-fullstack/index.js
@@ -34,7 +34,10 @@ if (process.env.NODE_ENV === 'production') {
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
+  // e.g.  if user want to visit emaily.com/surveys, it is not part of our api route
+  // we just return index.html, and in frontend webapp, it will look at the react-router in webapp and render that component 
   const path = require('path');
+  // this route handler is like a try catch block for the app in production, if all other places fail to match request
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
